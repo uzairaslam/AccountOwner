37ecc81f3e70035dbb9eebc50f7cef4a28da7e52
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2020-05-02 01:20:52 +0500

    final changes

--- EfCoreSeries/Controllers/ValuesController.cs
+++ EfCoreSeries/Controllers/ValuesController.cs
@@ -29,7 +29,11 @@ namespace EfCoreSeries.Controllers
             //var schemaName = entity.GetSchema();
             //var key = entity.FindPrimaryKey();
             //var properties = entity.GetProperties();
-            var students = _context.Students.Where(s => s.Age > 25).Include(st => st.Evaluations).ToList();
+            var students = _context.Students.Where(s => s.Age > 25)
+                .Include(st => st.Evaluations)
+                .Include(ss => ss.StudentSubjects)
+                .ThenInclude(s => s.Subject)
+                .FirstOrDefault();
             return Ok(students);
         }
     }
